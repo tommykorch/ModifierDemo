0dfb075d5700ed86c1592e84b6cfef776353ed87
tommykorch <tkorchmit@bk.ru>
2025-09-24 23:38:40 +0700

    Rounded corners photo

--- app/src/main/java/com/example/modifierdemo/MainActivity.kt
+++ app/src/main/java/com/example/modifierdemo/MainActivity.kt
@@ -23,6 +23,10 @@ import androidx.compose.foundation.Image
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.Alignment
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -55,12 +59,19 @@ fun DemoScreen(modifier: Modifier = Modifier) {
             fontWeight = FontWeight.Bold
         )
         Spacer(Modifier.height(16.dp))
-        CustomImage(R.drawable.vacation)
+        CustomImage(R.drawable.vacation,
+            Modifier
+                .padding(16.dp)
+                .width(270.dp)
+                .clip(shape = RoundedCornerShape(30.dp))
+        )
+
+
     }
 
 
 }
-@Preview(showBackground = true, showSystemUi = true)
+@Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     ModifierDemoTheme {
@@ -72,7 +83,7 @@ fun CustomImage(image: Int, modifier: Modifier = Modifier) {
     Image(
         painter = painterResource(image),
         contentDescription = null,
-        modifier
-
+        contentScale = ContentScale.Crop,
+        modifier = modifier
     )
 }
